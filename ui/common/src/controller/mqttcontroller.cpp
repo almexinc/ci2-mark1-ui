498fe296bcaf80b48d01160479508bf1976c57cd
ts-goto <ts-goto@usen-next.jp>
2025-05-16 16:51:32 +0900

    MQTTのリンクと実際にビルドの成功

---
+++ ui/common/src/controller/mqttcontroller.cpp
@@ -0,0 +1,13 @@
+﻿#include "mqttcontroller.h"
+
+MqttController::MqttController()
+{
+    this->_qMqttClient.setAutoKeepAlive(true);
+    this->_qMqttClient.setKeepAlive(1000);
+}
+
+MqttController *MqttController::getInstance()
+{
+    static MqttController instance;
+    return &instance;
+}
---
+++ ui/common/src/controller/mqttcontroller.h
@@ -0,0 +1,26 @@
+﻿#ifndef MQTTCONTROLLER_H
+#define MQTTCONTROLLER_H
+
+#include <QObject>
+#include <QtMqtt/qmqttclient.h>
+
+class MqttController : public QObject
+{
+    Q_OBJECT
+public:
+    MqttController(const MqttController &)            = delete;
+    MqttController &operator=(const MqttController &) = delete;
+    MqttController(MqttController &&)                 = delete;
+    MqttController &operator=(MqttController &&)      = delete;
+
+    static MqttController *getInstance();
+
+signals:
+
+private:
+    MqttController();
+
+    QMqttClient _qMqttClient;
+};
+
+#endif // MQTTCONTROLLER_H
