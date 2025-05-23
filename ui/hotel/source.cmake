c8aee81cbf4473a05f80a34e7fda7a57179a7326
ts-goto <ts-goto@usen-next.jp>
2025-05-23 18:23:07 +0900

    不要となった当初のサンプル実装を削除

--- ui/hotel/source.cmake
+++ ui/hotel/source.cmake
@@ -1,9 +1,4 @@
 ﻿set(HOTEL_BUILD_CPP_SOURCES
-    src/views/m0_0_0_initpage.cpp
-    src/views/m0_0_0_initpage.h
-    src/views/te0_0_1_testidle.cpp
-    src/views/te0_0_1_testidle.h
-
     src/views/sf0_0_initpage.cpp
     src/views/sf0_0_initpage.h
 
